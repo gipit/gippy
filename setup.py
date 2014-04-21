46bc42e91a7a8ddb6b8e272088cb9d80383ab445
Matthew Hanson <mhanson@appliedgeosolutions.com>
2014-04-21 16:49:28 -0400

    final setup changes...for now, handles install develop and bdist_egg

--- setup.py
+++ setup.py
@@ -24,13 +24,35 @@ setup for GIP and gippy
 
 import os
 import shutil
+import glob
 from setuptools import setup, Extension
+from setuptools.extension import Library
 from setuptools.command.install import install
 from setuptools.command.develop import develop
-from copy import deepcopy
+from setuptools.command.bdist_egg import bdist_egg
+from setuptools.command.build_ext import build_ext
 import numpy
 
-from pdb import set_trace
+
+def add_reg(filename):
+    # Have GDAL register file formats on import
+    f = open(filename, 'a')
+    f.write('reg()\n')
+    f.close()
+
+
+class gippy_build_ext(build_ext):
+    def finalize_options(self):
+        build_ext.finalize_options(self)
+        gippy_module.library_dirs.append(self.build_lib)
+
+
+class gippy_bdist_egg(bdist_egg):
+    def run(self):
+        self.distribution.ext_modules = [gip_module_static, gippy_module]
+        self.run_command('build_ext')
+        add_reg('gippy.py')
+        bdist_egg.run(self)
 
 
 class gippy_install(install):
@@ -39,26 +61,38 @@ class gippy_install(install):
         gippy_module.runtime_library_dirs.append(self.install_lib)
 
     def run(self):
-        os.system('cd GIP; make; cd ..')
         # ensure swig extension built before packaging
         self.run_command('build_ext')
         install.run(self)
-        shutil.copy('GIP/bin/Release/libgip.so', self.install_lib)
-        fname = os.path.join(self.install_lib, 'gippy.py')
-        # Have GDAL register file formats on import
-        f = open(fname, 'a')
-        f.write('reg()')
-        f.close()
+        add_reg(os.path.join(self.install_lib, 'gippy.py'))
 
 
 class gippy_develop(develop):
     def finalize_options(self):
         develop.finalize_options(self)
-        gippy_module.runtime_library_dirs = [os.path.abspath('GIP/bin/Release')]
+        gippy_module.runtime_library_dirs.append(os.path.abspath('./'))
 
     def run(self):
-        os.system('cd GIP; make; cd ..')
         develop.run(self)
+        add_reg('gippy.py')
+
+# Static library
+gip_module_static = Library(
+    name='gip',
+    sources=glob.glob('GIP/*.cpp'),
+    include_dirs=['GIP'],
+    language='c++',
+    extra_compile_args=['-std=c++0x', '-O3'],
+)
+
+# Dynamic shared library
+gip_module = Extension(
+    name='libgip',
+    sources=glob.glob('GIP/*.cpp'),
+    include_dirs=['GIP'],
+    language='c++',
+    extra_compile_args=['-std=c++0x', '-O3'],
+)
 
 gippy_module = Extension(
     name='_gippy',
@@ -66,7 +100,6 @@ gippy_module = Extension(
     swig_opts=['-c++', '-w509', '-IGIP'],  # '-keyword'],
     include_dirs=['GIP', numpy.get_include()],
     libraries=['gip', 'gdal', 'boost_system', 'boost_filesystem'],  # ,'X11'],
-    library_dirs=['GIP/bin/Release'],
     extra_compile_args=['-fPIC'],  # , '-std=c++0x'],
 )
 
@@ -77,11 +110,13 @@ setup(
     author='Matthew Hanson',
     author_email='mhanson@appliedgeosolutions.com',
     license='GPLv2',
-    ext_modules=[gippy_module],
+    ext_modules=[gip_module, gippy_module],
     py_modules=['gippy'],
-    #install_requires = ['','numpy'],
+    #install_requires = ['numpy'],
     cmdclass={
         "develop": gippy_develop,
         "install": gippy_install,
+        "bdist_egg": gippy_bdist_egg,
+        "build_ext": gippy_build_ext,
     }
 )
