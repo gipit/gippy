79dcec15d0dec2a676be4b5759cead37a6de8595
Matthew Hanson <mhanson@appliedgeosolutions.com>
2014-06-04 13:01:34 -0400

    added license header to all files

--- GIP/gip/GeoImage.h
+++ GIP/gip/GeoImage.h
@@ -1,3 +1,23 @@
+#!/usr/bin/env python
+################################################################################
+#    GIPPY: Geospatial Image Processing library for Python
+#
+#    Copyright (C) 2014 Matthew A Hanson
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see <http://www.gnu.org/licenses/>
+################################################################################
+
 #ifndef GIP_GEOIMAGE_H
 #define GIP_GEOIMAGE_H
 
