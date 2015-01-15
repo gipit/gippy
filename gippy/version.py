41b0f07a0642d28ea011b1894d7af9718a45dc63
Matthew Hanson <mhanson@appliedgeosolutions.com>
2015-01-15 15:36:45 -0500

    beta version bump

--- gippy/version.py
+++ gippy/version.py
@@ -17,4 +17,4 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program. If not, see <http://www.gnu.org/licenses/>
 ##############################################################################
-__version__ = '1.1.0b5'
+__version__ = '1.1.0b6'
