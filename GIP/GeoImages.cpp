9c64c83df9a2a970dccd766090a283c17febd90f
Matthew Hanson <mhanson@appliedgeosolutions.com>
2015-02-03 22:21:42 -0500

    added GeoImages class as collectin of GeoIage instances

---
+++ GIP/GeoImages.cpp
@@ -0,0 +1,37 @@
+/*##############################################################################
+#    GIPPY: Geospatial Image Processing library for Python
+#
+#    Copyright (C) 2015 Applied Geosolutions
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+##############################################################################*/
+
+#include <gip/GeoImages.h>
+#include <gip/geometry.h>
+
+namespace gip {
+    using std::vector;
+
+    // find transformed union of all raster bounding boxes
+    Rect<double> GeoImages::Extent(OGRSpatialReference srs) const {
+        Rect<double> ext;
+        vector< Rect<double> > extents;
+        for (vector<GeoImage>::const_iterator i=_GeoImages.begin(); i!=_GeoImages.end(); i++) {
+            ext = i->Extent();
+            ext.Transform(i->SRS(), srs);
+            extents.push_back(ext);
+        }
+        return Union<double>(extents);
+    }
+
+}
