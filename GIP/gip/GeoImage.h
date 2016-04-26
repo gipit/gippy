88fef8b2fb9db440e5fa6fb8725b0e863136e193
Matthew Hanson <matt.a.hanson@gmail.com>
2016-04-26 18:47:55 -0400

    change dt keyword to datatype in GeoImage.save

--- GIP/gip/GeoImage.h
+++ GIP/gip/GeoImage.h
@@ -403,11 +403,11 @@ namespace gip {
     */
 
     // Save input file with processing applied into new output file
-    template<class T> GeoImage GeoImage::save(std::string filename, std::string dt, bool overviews) const {
+    template<class T> GeoImage GeoImage::save(std::string filename, std::string datatype, bool overviews) const {
         // TODO: if not supplied base output datatype on units?
-        if (dt == "") dt = this->type().string();
+        if (datatype == "") datatype = this->type().string();
 
-        GeoImage imgout = GeoImage::create_from(*this, filename, nbands(), dt);
+        GeoImage imgout = GeoImage::create_from(*this, filename, nbands(), datatype);
         for (unsigned int i=0; i<imgout.nbands(); i++) {
             (*this)[i].save<T>(imgout[i]);
         }
