846786790c537182781dca6e0c715c3796d1e5e4
Matthew Hanson <matt.a.hanson@gmail.com>
2016-12-30 01:19:19 -0500

    misc

--- GIP/gip/geometry.h
+++ GIP/gip/geometry.h
@@ -66,8 +66,9 @@ namespace gip {
             OGRSpatialReference _dst;
             _dst.SetFromUserInput(dst.c_str());
             OGRCoordinateTransformation* trans = OGRCreateCoordinateTransformation(&_src, &_dst);
-            double newx(_x), newy(_y);
+            double newx(x()), newy(y());
             trans->Transform(1, &newx, &newy);
+            OCTDestroyCoordinateTransformation(trans);
             return Point<T>(newx, newy);
         }
 
